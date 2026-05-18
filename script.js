718685aaa47495c12f0737cfa1f07259d0a2e04c
Rimee Srivastava <rimee1912@>
2026-05-18 15:48:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const slider  =  document.querySelector(".slider");
+document.querySelector(".next").onclick = () => {
+    slider.scrollBy({left : 300, behavior: "smooth"});
+
+};
+
+document.querySelector(".prev").onclick = () => {
+    slider.scrollBy({left: -300, behavior: "smooth"});
+};
+
+
+
+
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach((q) => {
+    q.addEventListener("click", () => {
+        const answer = q.nextElementSibling;
+        const icon = q.querySelector(".icon");
+
+        if (answer.style.maxHeight) {
+            answer.style.maxHeight = null;
+            icon.textContent = "+";
+        } else {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+            icon.textContent = "×";
+        }
+    });
+});
